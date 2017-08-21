8990c1239548d067e9502a62b98853d86a4b14ad
seansio1995 <seansio@163.com>
2017-08-21 18:00:02 -0400

    updates

--- demo.py
+++ demo.py
@@ -1,4 +1,47 @@
+from PIL import Image
 char_string = 'abcdefghijklmnopqrstuvwxyz'
 
 def rgb2char(r,g,b):
-    
+    length=len(char_string)
+    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+
+    unit=(256.0+1)/length;
+
+    ##the corresponding char from r,g,b
+
+    index=int(gray/unit)
+    return char_string[index]
+
+
+def preprocess(image_path,delta=100):
+    img=Image.open(image_path)
+    height,width=img.size
+
+    if height > width:
+        max_val=height
+
+    else:
+        max_val=width
+
+
+    scale=max_val/100
+
+    width, height=int(width/scale), int(height/scale)
+
+    img=img.resize((width,height))
+
+
+    return img
+
+
+
+def img_to_char(img_object, save_path):
+    text=""
+    width,height=img_object.size
+
+
+    for i in range(height):
+        line=""
+        for j in range(width):
+            line+=rgb2char(*img_object.getpixel((j,i)))
+        text
